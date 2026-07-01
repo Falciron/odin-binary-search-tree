0d121e897a41af7efaa5b0e1fa45fa6e08edbb29
Falciron <2159742+Falciron@users.noreply.github.com>
2026-07-01 12:46:41 -0700

    Implement build_tree with non-existent method call
    A tree builds itself. We should be building a tree fort.

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,5 +1,20 @@
 class Tree
   def initialize(numeric_array)
-    @root = nil
+    @root = build_tree(numeric_array)
+  end
+
+  private
+
+  def build_tree(numeric_array)
+    numeric_array.each { |numeric_value| insert(numeric_value) }
+    @root
+  end
+
+  def pretty_print(node = @root, prefix = '', is_left: true)
+    return unless node
+
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", is_left: false)
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", is_left: true)
   end
 end
